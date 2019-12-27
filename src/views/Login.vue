2795c72477c1e1afe00b4cee2487b1ac45243eb6
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-12-27 19:00:38 +0530

    Add sign in by email link

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,14 +2,13 @@
   <v-container fluid>
     <v-layout justify-center wrap class="my-4" align-center>
       <section>
-        <v-btn
-          :loading="isLoading"
-          :disabled="isLoading"
-          x-large
-          @click="login"
-        >
+        <v-btn class="ma-2" x-large @click="loginByGoogle">
           <v-icon color="primary">{{ mdiGoogle }}</v-icon>
           Login using Google
+        </v-btn>
+        <v-btn class="ma-2" x-large @click="loginByEmailLink">
+          <v-icon color="accent">{{ mdiEmail }}</v-icon>
+          Login using email link
         </v-btn>
         <p>
           <strong>Note</strong>
@@ -24,35 +23,74 @@
         <p>Check out the <a href="/faq">FAQ</a> page for more details.</p>
       </section>
     </v-layout>
+    <v-overlay :value="isLoading">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </v-container>
 </template>
 
 <script>
 import firebase from "firebase/app";
 import "firebase/auth";
-import { mdiGoogle } from "@mdi/js";
+import { mdiGoogle, mdiEmail } from "@mdi/js";
 
 export default {
   name: "Login",
   data() {
     return {
       mdiGoogle,
-      authProvider: new firebase.auth.GoogleAuthProvider(),
+      mdiEmail,
       isLoading: false
     };
   },
   created: function() {
     this.isLoading = true;
-    firebase.auth().onAuthStateChanged(user => {
-      this.isLoading = false;
-      if (user) {
-        this.$router.replace("/record");
+
+    if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+      var email = window.localStorage.getItem("MSCEmailForSignIn");
+      if (email) {
+        firebase
+          .auth()
+          .signInWithEmailLink(email, window.location.href)
+          .then(user => {
+            this.isLoading = false;
+            if (user) {
+              this.$router.replace("/record");
+            }
+          });
       }
-    });
+    } else {
+      // Google sign in
+      firebase.auth().onAuthStateChanged(user => {
+        this.isLoading = false;
+        if (user) {
+          this.$router.replace("/record");
+        }
+      });
+    }
   },
   methods: {
-    login() {
-      firebase.auth().signInWithRedirect(this.authProvider);
+    loginByGoogle() {
+      firebase
+        .auth()
+        .signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    },
+    loginByEmailLink() {
+      let email = window.localStorage.getItem("MSCEmailForSignIn");
+      const actionCodeSettings = {
+        // URL you want to redirect back to. The domain (www.example.com) for this
+        // URL must be whitelisted in the Firebase Console.
+        url: window.location.href,
+        handleCodeInApp: true
+      };
+
+      if (!email) {
+        // User opened the link on a different device. To prevent session fixation
+        // attacks, ask the user to provide the associated email again. For example:
+        email = window.prompt("Please provide your email for confirmation");
+      }
+      window.localStorage.setItem("MSCEmailForSignIn", email);
+      firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings);
     }
   }
 };
